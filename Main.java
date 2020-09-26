de19c582ec207c31bf0a2cb5125110809f06f9ab
Darrel Supreme <darrel.supreme@patelhs.org>
2020-09-26 22:19:46 +0000

    I created an if/else statement that compares my birthday to todays date to my birth year.

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+class CheckMyAge {
+
+  public static void main(String[] args) 
+  {
+    int myAge= 15;
+    int myBirthday= 04;
+    int todaysDate= 926;
+    if(myBirthday == todaysDate) 
+    {
+      System.out.println("I am myAge++ years old");
+    }else{
+      System.out.println("I am still 15 years old");
+    }
   }
 }
